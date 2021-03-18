38fc90842848daef3c05ee03114aebd388e9fd1b
FreedMeet <freedmeetme@gmail.com>
2021-03-18 20:37:17 +0200

    Another structure and some changes

--- gulpfile.js
+++ gulpfile.js
@@ -13,71 +13,90 @@ const htmlbeautify = require('gulp-html-beautify');
 const imagemin = require('gulp-imagemin');
 
 const styleFiles = [
-   './src/styles/*.less',
+    './src/styles/*.less',
 ]
 
 const scriptFiles = [
-   './src/js/*.js',
+    './src/scripts/*.js',
 ]
 
-gulp.task('beauty', () => {
-    gulp.src('./build/*.html')
-      .pipe(htmlbeautify({indentSize: 2}))
-      .pipe(gulp.dest('./build'))
+const htmlFiles = [
+    './src/*.html',
+]
+
+const fonts = [
+    './src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}',
+]
+
+gulp.task('html_main', () => {
+    return gulp.src(htmlFiles)
+        .pipe(concat('index.html'))
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('styles', () => {
-
-   return gulp.src(styleFiles)
-      .pipe(sourcemaps.init())
-      //Choice stylus() , sass() или less()
-      .pipe(less())
-      .pipe(concat('style.css'))
-      .pipe(autoprefixer({
-         overrideBrowserslist: ['last 2 versions'],
-         cascade: false
-      }))
-      .pipe(cleanCSS({
-         level: 2
-      }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build/css'))
-      .pipe(browserSync.stream());
+    return gulp.src(styleFiles)
+        .pipe(sourcemaps.init())
+        //Choice stylus() , sass() или less()
+        .pipe(less())
+        .pipe(concat('style.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./build/styles'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', () => {
-
-   return gulp.src(scriptFiles)
-      .pipe(concat('script.js'))
-      .pipe(uglify({
-         toplevel: true
-      }))
-      .pipe(gulp.dest('./build/js'))
-      .pipe(browserSync.stream());
-});
-
-gulp.task('del', () => {
-   return del(['build/*'])
+    return gulp.src(scriptFiles)
+        .pipe(concat('script.js'))
+        .pipe(uglify({
+            toplevel: true
+        }))
+        .pipe(gulp.dest('./build/scripts'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('img-compress', () => {
-   return gulp.src('./src/img/**')
-   .pipe(imagemin({
-      progressive: true
-   }))
-   .pipe(gulp.dest('./build/img/'))
+    return gulp.src('./src/assets/img/**')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('./build/assets/img/'))
+});
+
+gulp.task('beauty', () => {
+    gulp.src('./build/*.html')
+        .pipe(htmlbeautify({indentSize: 2}))
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task('fonts', () => {
+    gulp.src('./src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}')
+        .pipe(gulp.dest('./build/assets/fonts/'));
+});
+
+gulp.task('del', () => {
+    return del(['build/*'])
 });
 
 gulp.task('watch', () => {
-   browserSync.init({
-      server: {
-         baseDir: "./build"
-      }
-   });
-   gulp.watch('./src/img/**', gulp.series('img-compress'))
-   gulp.watch('./src/styles/**/*.less', gulp.series('styles'))
-   gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
-   gulp.watch("./*.html").on('change', browserSync.reload)
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    gulp.watch('./src/assets/img/**', gulp.series('img-compress'))
+    gulp.watch('./src/styles/**/*.{css,stylus,less,sass,scss}', gulp.series('styles'))
+    gulp.watch('./src/scripts/**/*.js', gulp.series('scripts'))
+    gulp.watch('./src/**/*.html', gulp.series('html_main'))
+    gulp.watch("./**/*.html").on('change', browserSync.reload)
 });
 
-gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress'), 'watch'));
+gulp.task('default', gulp.series('del', gulp.parallel('html_main', 'styles', 'scripts', 'img-compress'), 'watch'));
