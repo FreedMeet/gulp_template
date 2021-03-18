a4085a4a2cf0bd4e97f41a2d8bfa828f050a88cf
FreedMeet <freedmeetme@gmail.com>
2021-03-18 19:25:56 +0200

    Changed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-//uncomment what you need
-
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
@@ -22,64 +20,6 @@ const scriptFiles = [
    './src/js/*.js',
 ]
 
-const htmlHomeFiles = [
-   './src/html/head_and_styles.html',
-   './src/html/home_page/*.html',
-   './src/html/end_and_scripts.html',
-]
-
-gulp.task('html_main', () =>{
-   return gulp.src(htmlHomeFiles)
-      .pipe(concat('home_page.html'))
-      .pipe(gulp.dest('./build'))
-      .pipe(browserSync.stream());
-});
-
-//-------------------
-//================
-// const htmlAuthFiles = [
-//    './src/html/head_and_styles.html',
-//    './src/html/auth_and_reg/auth.html',
-//    './src/html/end_and_scripts.html',
-// ]
-
-// gulp.task('html_auth', () =>{
-//    return gulp.src(htmlAuthFiles)
-//       .pipe(concat('auth_page.html'))
-//       .pipe(gulp.dest('./build'))
-//       .pipe(browserSync.stream());
-// });
-
-
-// const htmlRegFiles = [
-//    './src/html/head_and_styles.html',
-//    './src/html/auth_and_reg/reg.html',
-//    './src/html/end_and_scripts.html',
-// ]
-
-// gulp.task('html_reg', () =>{
-//    return gulp.src(htmlRegFiles)
-//       .pipe(concat('reg_page.html'))
-//       .pipe(gulp.dest('./build'))
-//       .pipe(browserSync.stream());
-// });
-
-
-// const htmlProfileFiles = [
-//    './src/html/head_and_styles.html',
-//    './src/html/auth_and_reg/profile.html',
-//    './src/html/end_and_scripts.html',
-// ]
-
-// gulp.task('html_profile', () =>{
-//    return gulp.src(htmlProfileFiles)
-//       .pipe(concat('profile_page.html'))
-//       .pipe(gulp.dest('./build'))
-//       .pipe(browserSync.stream());
-// });
-//================
-//-------------------
-
 gulp.task('beauty', () => {
     gulp.src('./build/*.html')
       .pipe(htmlbeautify({indentSize: 2}))
@@ -90,7 +30,7 @@ gulp.task('styles', () => {
 
    return gulp.src(styleFiles)
       .pipe(sourcemaps.init())
-      //Choise stylus() , sass() или less()
+      //Choice stylus() , sass() или less()
       .pipe(less())
       .pipe(concat('style.css'))
       .pipe(autoprefixer({
@@ -137,19 +77,7 @@ gulp.task('watch', () => {
    gulp.watch('./src/img/**', gulp.series('img-compress'))
    gulp.watch('./src/styles/**/*.less', gulp.series('styles'))
    gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
-   gulp.watch('./src/html/**/*.html', gulp.series('html_main'))
-   // gulp.watch('./src/html/**/*.html', gulp.series('html_auth'))
-   // gulp.watch('./src/html/**/*.html', gulp.series('html_reg'))
-   // gulp.watch('./src/html/**/*.html', gulp.series('html_profile'))
    gulp.watch("./*.html").on('change', browserSync.reload)
 });
 
-// const htmlAd = [
-//    'html_auth',
-//    'html_reg',
-//    'html_profile',
-// ]
-
-const htmlAd = []
-
-gulp.task('default', gulp.series('del', gulp.parallel('html_main', htmlAd, 'styles', 'scripts', 'img-compress'), 'watch'));
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress'), 'watch'));
