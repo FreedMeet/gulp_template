28bf6782f26dfb300041fb620cb8358a2308d0e9
FreedMeet <freedmeetme@gmail.com>
2021-04-10 23:11:40 +0300

    Changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,107 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const sourcemaps = require('gulp-sourcemaps');
-// const stylus = require('gulp-stylus');
-// const sass = require('gulp-sass');
-const less = require('gulp-less');
-const htmlbeautify = require('gulp-html-beautify');
-const imagemin = require('gulp-imagemin');
-const plumber = require('gulp-plumber')
-const pug = require('gulp-pug')
+let preprocessor = "less";
 
-const styleFiles = [
-    './src/styles/*.less',
-]
+const { src, dest, series, parallel, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const del = require("del");
+const sourcemaps = require("gulp-sourcemaps");
+const stylus = require("gulp-stylus");
+const sass = require("gulp-sass");
+const less = require("gulp-less");
+const htmlbeautify = require("gulp-html-beautify");
+const imagemin = require("gulp-imagemin");
+const plumber = require("gulp-plumber");
+const pug = require("gulp-pug");
+const newer = require('gulp-newer')
 
-const scriptFiles = [
-    './src/scripts/*.js',
-]
+function browsersync() {
+  browserSync.init({
+    server: { baseDir: "./build" },
+    notify: false,
+    online: true,
+  });
+}
 
-const pugFiles = [
-    './src/pages/*.pug',
-]
+function scripts() {
+  return src("./src/scripts/**/*.js")
+    .pipe(plumber())
+    .pipe(concat("script.min.js"))
+    .pipe(uglify({ toplevel: true }))
+    .pipe(dest("./build/scripts"))
+    .pipe(browserSync.stream());
+}
 
-const fonts = [
-    './src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}',
-]
+function styles() {
+  return src("./src/styles/**/*." + preprocessor)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(eval(preprocessor)())
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true })
+    )
+    .pipe(cleanCSS({ level: { 1: { specialComments: 0 } } }))
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest("./build/styles"))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('pugToHtml', () => {
-    return gulp.src(pugFiles)
-        .pipe(plumber())
-        .pipe(pug())
-        .pipe(gulp.dest('build'))
-});
+function pugToHtml() {
+  return src("./src/pages/*.pug")
+    .pipe(plumber())
+    .pipe(pug())
+    .pipe(dest("build"))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('styles', () => {
-    return gulp.src(styleFiles)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        //Choice stylus() , sass() или less()
-        .pipe(less())
-        .pipe(concat('style.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./build/styles'))
-        .pipe(browserSync.stream());
-});
+function img_compress() {
+  return src("./src/assets/img/**/*")
+    .pipe(newer("./build/assets/img/"))
+    .pipe(imagemin())
+    .pipe(dest("./build/assets/img/"));
+}
 
-gulp.task('scripts', () => {
-    return gulp.src(scriptFiles)
-        .pipe(concat('script.js'))
-        .pipe(uglify({
-            toplevel: true
-        }))
-        .pipe(gulp.dest('./build/scripts'))
-        .pipe(browserSync.stream());
-});
+function clean_img() {
+  return del("./build/assets/img/**/*", { force: true });
+}
 
-gulp.task('img-compress', () => {
-    return gulp.src('./src/assets/img/**')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./build/assets/img/'))
-});
+function clean_build() {
+    return del("./build", { force: true });
+  }
 
-gulp.task('beauty', () => {
-    gulp.src('./build/*.html')
-        .pipe(htmlbeautify({indentSize: 2}))
-        .pipe(gulp.dest('./build'))
-});
+function fonts() {
+  return src("./src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}").pipe(
+    dest("./build/assets/fonts/")
+  );
+}
 
-gulp.task('fonts', () => {
-    gulp.src(fonts)
-        .pipe(gulp.dest('./build/assets/fonts/'));
-});
+function beauty() {
+  return src("./build/*.html")
+    .pipe(htmlbeautify({ indentSize: 2 }))
+    .pipe(dest("./build"));
+}
 
-gulp.task('del', () => {
-    return del(['build/*'])
-});
+function startwatch() {
+    watch("./src/scripts/**/.js", scripts);
+    watch("./src/styles/**/*." + preprocessor, styles);
+    watch("./src/pages/**/*.pug", pugToHtml);
+    watch("./src/assets/img/**/*", img_compress);
+    watch("./src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}", fonts)
+}
 
-gulp.task('watch', () => {
-    browserSync.init({
-        server: {
-            baseDir: "./build"
-        }
-    });
-    gulp.watch('./src/assets/img/**', gulp.series('img-compress'))
-    gulp.watch('./src/styles/**/*.{css,stylus,less,sass,scss}', gulp.series('styles'))
-    gulp.watch('./src/scripts/**/*.js', gulp.series('scripts'))
-    gulp.watch('./src/**/*.pug', gulp.series('pugToHtml'))
-    gulp.watch("./**/*.html").on('change', browserSync.reload)
-});
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.pugToHtml = pugToHtml;
+exports.img_compress = img_compress;
+exports.clean_img = clean_img;
+exports.clean_build = clean_build
+exports.fonts = fonts;
+exports.beauty = beauty;
 
-gulp.task('default', gulp.series('del', gulp.parallel('pugToHtml', 'styles', 'scripts', 'img-compress'), 'watch'));
+exports.build = series(clean_build, pugToHtml, img_compress, styles, scripts, fonts);
+
+exports.default = parallel(styles, scripts, pugToHtml, browsersync, startwatch);
